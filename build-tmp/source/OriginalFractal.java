4390f02739176da0347ba24c2e7ac2e51ba1a763
unknown <jedelrio@697-334-Upbourn.sf.k12.internal>
2015-02-12 11:07:41 -0800

    Original Fractal h2o

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
 public int smallCircle =100;
 private int someNumber = 20; 
 //private double newW = 300; 
@@ -12,7 +28,7 @@ public void draw(){
     background(0);
 int randNumX =(int)(Math.random()*700);
 int randNumY = (int)(Math.random()*700);
-    drawCircle(randNumX,randNumY, 10,.30);
+    drawCircle(randNumX,randNumY, 10,.30f);
 
  }
 	
@@ -58,3 +74,12 @@ public void drawCircle(int myX, int myY, int something, double somefraction){
     rect(30,30,30,30);
   }
 }*/
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
